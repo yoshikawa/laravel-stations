4aba23ffc1d92bdbcc2834a40f79818949844865
Taiki Yoshikawa <yoshikawataiki@gmail.com>
2022-08-27 15:58:18 +0900

    update chapter4

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,4 @@ Route::get('/', function () {
 Route::get('/practice', [PracticeController::class, 'sample']);
 Route::get('/practice2', [PracticeController::class, 'sample2']);
 Route::get('/practice3', [PracticeController::class, 'sample3']);
+Route::get('/getPractice', [PracticeController::class, 'getPractice']);
