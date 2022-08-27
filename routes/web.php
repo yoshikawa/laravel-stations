fff0d6bc985dadd02c614990122ba9ab3411ca7c
Taiki Yoshikawa <yoshikawataiki@gmail.com>
2022-08-27 15:39:37 +0900

    update routes, controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PracticeController;
 use Illuminate\Support\Facades\Route;
 /*
 |-------------------------------------------------------------------------
@@ -15,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/practice', [PracticeController::class, 'sample']);
+Route::get('/practice2', [PracticeController::class, 'sample2']);
+Route::get('/practice3', [PracticeController::class, 'sample3']);
