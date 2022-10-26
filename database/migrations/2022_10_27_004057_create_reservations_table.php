05da82c72025941309e856f278fd4e86831a8e8f
Taiki Yoshikawa <yoshikawataiki@gmail.com>
2022-10-27 00:43:36 +0900

    [追加] 座席予約のリンクをつくった、ファイル生成

---
+++ database/migrations/2022_10_27_004057_create_reservations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->date('screening_date');
+            $table->unsignedBigInteger('schedule_id');
+            $table->unsignedBigInteger('sheet_id');
+            $table->string('email', '255');
+            $table->string('name', '255');
+            $table->timestamps();
+
+            $table->foreign('schedule_id')->references('id')->on('schedules')->cascadeOnDelete();
+            $table->foreign('sheet_id')->references('id')->on('sheets');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
